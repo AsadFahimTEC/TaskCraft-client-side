fe80c02be67f818d96dc258a32b91041c187dbe8
AsadFahimTEC <asaduzzamanfahim838@gmail.com>
2023-11-28 19:07:08 +0600

    booking form design finish properly

--- src/Service/ServiceDetails/BookingForm.jsx
+++ src/Service/ServiceDetails/BookingForm.jsx
@@ -1,29 +1,48 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import DatePicker from "react-datepicker";
 import Select from "react-select";
 import "react-datepicker/dist/react-datepicker.css";
+import { AuthContext } from "../../Hook/AuthProvider";
+import { useLoaderData } from "react-router-dom";
 
-const BookingForm = ({ packageDetails, guideList }) => {
+const BookingForm = ({ packagee, guideList }) => {
   const [touristName, setTouristName] = useState("");
   const [touristEmail, setTouristEmail] = useState("");
   const [touristImage, setTouristImage] = useState(null);
   const [tourDate, setTourDate] = useState(null);
   const [selectedGuide, setSelectedGuide] = useState(null);
+  const { user } = useContext(AuthContext);
 
-  const handleTouristImageChange = (event) => {
-    const file = event.target.files[0];
-    // Process the file as needed
-    setTouristImage(file);
-  };
+  const {
+    spotPhoto,
+    spotPhoto2,
+    spotPhoto3,
+    spotPhoto4,
+    spotPhoto5,
+    tourType,
+    tripTitle,
+    day,
+    location,
+    tourGuide,
+    price,
+  } = packagee || {};
+  const packages = useLoaderData();
+  console.log(packages, packages[0].price);
+
+  // const handleTouristImageChange = (event) => {
+  //   const file = event.target.files[0];
+  //   // Process the file as needed
+  //   setTouristImage(file);
+  // };
 
   const handleBookNow = () => {
     // Implement the logic to handle the booking
     console.log({
-      packageName: packageDetails?.name,
-      touristName,
-      touristEmail: packageDetails?.email,
-      touristImage,
-      price: packageDetails?.price,
+      packageName: packages?.tourType,
+      touristName: user?.displayName,
+      touristEmail: user?.email,
+      touristImage: user?.photoURL,
+      price: packages?.price,
       tourDate,
       selectedGuide,
     });
@@ -39,49 +58,39 @@ const BookingForm = ({ packageDetails, guideList }) => {
       <div className="max-w-lg mx-auto bg-white rounded-md overflow-hidden shadow-lg p-6">
         <h2 className="text-2xl font-bold mb-4">Booking Form</h2>
         <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">
-            Name of the Package
+          <label className="flex flex-col text-gray-700 text-sm font-bold mb-2">
+            Name of the Package:
+            <p className="mt-2">{packages[0].tourType}</p>
           </label>
-          <p>{packageDetails?.name}</p>
+        </div>
+
+        <div className="mb-4">
+          <label className="flex gap-5 text-gray-700 text-sm font-bold mb-2">
+            Tourist Name: <br />
+            {user.displayName}
+          </label>
         </div>
         <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">
-            Tourist Name
+          <label className="flex gap-5 text-gray-700 text-sm font-bold mb-2">
+            Tourist Email: <br />
+            {user.email}
           </label>
-          <input
-            type="text"
-            className="w-full p-2 border border-gray-300 rounded"
-            value={touristName}
-            onChange={(e) => setTouristName(e.target.value)}
-          />
         </div>
         <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">
-            Tourist Email
+          <label className="text-gray-700 text-sm font-bold mb-2">
+            Tourist Image:
           </label>
-          <input
-            type="email"
-            className="w-full p-2 border border-gray-300 rounded"
-            value={touristEmail}
-            onChange={(e) => setTouristEmail(e.target.value)}
-          />
+          <br />
+          <img className="rounded-lg" src={user.photoURL} alt="" />
         </div>
+
         <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">
-            Tourist Image
+          <label className="flex flex-col gap-5 text-gray-700 text-sm font-bold mb-2">
+            Price:
+            <p>{packages[0].price}</p>
           </label>
-          <input
-            type="file"
-            accept="image/*"
-            onChange={handleTouristImageChange}
-          />
         </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">
-            Price
-          </label>
-          <p>{packageDetails?.price}</p>
-        </div>
+
         <div className="mb-4">
           <label className="block text-gray-700 text-sm font-bold mb-2">
             Tour Date
