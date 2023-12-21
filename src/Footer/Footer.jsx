056491f3eb5b695b5ac1ec6a9f5aa6a9d46c1fa1
AsadFahimTEC <asaduzzamanfahim838@gmail.com>
2023-12-21 11:46:31 +0600

    login authentication with google and footer with social media clickable link finished properly

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-import logo from "../assets/logo4.png";
 
 const Footer = () => {
   return (
@@ -7,15 +6,15 @@ const Footer = () => {
       <footer className="flex flex-col space-y-10 justify-center m-10">
         <nav className="flex justify-center flex-wrap gap-6 text-gray-500 font-medium">
           <h6 className="normal-case text-[#000] font-montserrat font-bold text-xl">
-            JourneyJive
+            TaskCraft
           </h6>
-          <div className="ml-4 -mt-6">
+          {/* <div className="ml-4 -mt-6">
             <img className="w-20 h-16 sm:w-24 sm:h-20" src={logo} alt="logo" />
-          </div>
+          </div> */}
           <Link className="hover:text-gray-900" to="/">
             Home
           </Link>
-          <Link className="hover:text-gray-900" to="/services">
+          <Link className="hover:text-gray-900" to="/sservices">
             Packages
           </Link>
           <Link className="hover:text-gray-900" to="/media">
@@ -25,43 +24,43 @@ const Footer = () => {
 
         <div className="flex justify-center space-x-5">
           <a
-            href="https://facebook.com"
+            href="https://www.facebook.com/Asaduzzamantec"
             target="_blank"
             rel="noopener noreferrer"
           >
             <img src="https://img.icons8.com/fluent/30/000000/facebook-new.png" />
           </a>
           <a
-            href="https://linkedin.com"
+            href="https://www.linkedin.com/in/md-asaduzzaman-a03757122"
             target="_blank"
             rel="noopener noreferrer"
           >
             <img src="https://img.icons8.com/fluent/30/000000/linkedin-2.png" />
           </a>
           <a
-            href="https://instagram.com"
+            href="https://www.instagram.com/ahfahim"
             target="_blank"
             rel="noopener noreferrer"
           >
             <img src="https://img.icons8.com/fluent/30/000000/instagram-new.png" />
           </a>
           <a
-            href="https://messenger.com"
+            href="https://m.me/Asaduzzamantec"
             target="_blank"
             rel="noopener noreferrer"
           >
             <img src="https://img.icons8.com/fluent/30/000000/facebook-messenger--v2.png" />
           </a>
-          <a
+          {/* <a
             href="https://twitter.com"
             target="_blank"
             rel="noopener noreferrer"
           >
             <img src="https://img.icons8.com/fluent/30/000000/twitter.png" />
-          </a>
+          </a> */}
         </div>
         <p className="text-center text-gray-700 font-medium">
-          &copy; 2023 PeoplePro Company Ltd. All rights reserved.
+          &copy; 2023 TaskCraft Company Ltd. All rights reserved.
         </p>
       </footer>
     </div>
--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,10 +1,7 @@
 import Sliders from "../Banner/Banner";
-import Contact from "../ContactUs/Contact";
-import ContactUs from "../ContactUs/ContactUs";
 // import Services from "../Services/Services";
 import UserSection from "../Tabs/Tabs";
-import TouristStories from "../TourStory/TourStory";
-import Type from "../Type/Type";
+
 
 
 
@@ -13,10 +10,6 @@ const Home = () => {
         <div>
             <Sliders></Sliders>
             <UserSection></UserSection>
-            <TouristStories></TouristStories>
-            <Contact></Contact>
-            <Type></Type>
-            <ContactUs></ContactUs>
         </div>
     );
 };
